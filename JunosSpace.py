27214e9f0c6649a2e1c1e3f3324dbc62cf845be3
Jason Nadeau <jason.nadeau@gmail.com>
2014-09-05 14:56:09 -0400

    intial commit

---
+++ JunosSpace.py
@@ -0,0 +1,34 @@
+__author__ = 'Jason Nadeau'
+
+
+class JunosSpace(object):
+    URI = '/api'
+
+    def __setHostname(self, hostname):
+        '''This will set the hostname for the JunosSD object it will run __setURLPath to update URL to reflect new
+        hostname'''
+        self.hostname = hostname
+        self.__setURLPath()
+
+    def __setURLPath(self):
+        '''This method will create the proper API URI stem for Junos Space Security Director'''
+        urlPathStr = '''https://{hostname}{URI}'''
+        urlDict = {'hostname': self.hostname,
+                   'URI': self.URI}
+        self.URL = urlPathStr.format(**urlDict)
+
+    def __init__(self, hostname):
+
+
+        self.__setHostname(hostname)
+        print(self.URL)
+
+class JunosSD(JunosSpace):
+    URI = JunosSpace.URI + '/juniper/sd'
+    def __init__(self,hostname):
+        super(JunosSD, self).__init__(hostname)
+
+
+
+
+

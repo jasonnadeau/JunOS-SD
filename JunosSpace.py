ebe40659d3359621ecb86411ec41657fdf70f8b8
Jason Nadeau (a269052) <jason.nadeau@fmr.com>
2014-09-08 09:55:20 -0400

    put __ini__ first in JunosSpace superclass.

--- JunosSpace.py
+++ JunosSpace.py
@@ -3,6 +3,11 @@ __author__ = 'Jason Nadeau'
 
 class JunosSpace(object):
     URI = '/api'
+    def __init__(self, hostname):
+
+
+            self.__setHostname(hostname)
+            print(self.URL)
 
     def __setHostname(self, hostname):
         '''This will set the hostname for the JunosSD object it will run __setURLPath to update URL to reflect new
@@ -11,18 +16,13 @@ class JunosSpace(object):
         self.__setURLPath()
 
     def __setURLPath(self):
-        '''This method will create the proper API URI stem for Junos Space Security Director'''
+        '''This method will create the proper API URI stem for Junos Space or Space application if used by subclass'''
         urlPathStr = '''https://{hostname}{URI}'''
         urlDict = {'hostname': self.hostname,
                    'URI': self.URI}
         self.URL = urlPathStr.format(**urlDict)
 
-    def __init__(self, hostname):
-
-
-        self.__setHostname(hostname)
-        print(self.URL)
-
+    
 class JunosSD(JunosSpace):
     URI = JunosSpace.URI + '/juniper/sd'
     def __init__(self,hostname):
